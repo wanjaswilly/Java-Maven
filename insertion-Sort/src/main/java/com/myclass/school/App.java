af487836689f738f02a7e2017b7ae11329a9ece5
sniper <wanjaswilly@gmail.com>
2022-03-14 16:57:08 +0300

    added the insertion sort

---
+++ insertion-Sort/src/main/java/com/myclass/school/App.java
@@ -0,0 +1,13 @@
+package com.myclass.school;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
